289a095028a27b1b25aa0d74301046863598f4a9
Alejandro Arias <alejandroarias@wireless-10-147-243-212.public.utexas.edu>
2021-10-20 14:43:04 -0500

    Solved TwoSum problem

---
+++ Java/Leetcode1TwoSum.java
@@ -0,0 +1,13 @@
+class Leetcode1TwoSum {
+    public int[] twoSum(int[] nums, int target) {   
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < nums.length; i++) {
+            if(map.containsKey(target - nums[i])) {
+                return new int[] {map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("Not found");
+    }
+}
